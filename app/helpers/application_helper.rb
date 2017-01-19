37487b0524cab5a4f6da287a97d466dce7d8b55a
QUANG VU <vubaquang123@gmail.com>
2017-01-19 17:04:24 +0700

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
-  def full_title(page_title = "")
+  def full_title page_title = ""
     base_title = "Ruby on Rails Tutorial Sample App"
-    page_title.empty? base_title : page_title + " | " + base_title
+    page_title.empty? ? base_title : page_title + " | " + base_title
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get  "static_pages/about"
   root "static_pages#home"
+  get  "static_pages/home"
+  get  "static_pages/help"
+  get  "static_pages/about"
+  get  "static_pages/contact"
+  get  "users/new"
 end
