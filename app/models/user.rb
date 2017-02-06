623d2d498a92669a7a5352f171dd35daad2192eb
QUANG VU <vubaquang123@gmail.com>
2017-02-06 13:46:10 +0700

    master

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  before_save { email.downcase! }
   validates :name,  presence: true, length:{maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length:{maximum:255},
@@ -8,4 +7,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, length:{minimum: 6}
 
+  def bf_save
+    before_save { email.downcase! }
+  end
 end
