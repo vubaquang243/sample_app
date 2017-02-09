11ca4931ca63f72d4cdb594d2081edf4a56dc37d
QUANG VU <vubaquang123@gmail.com>
2017-02-09 13:36:44 +0700

    Implement advanced login

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -7,6 +7,7 @@ class SessionController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] ? remember(user) : forget(user)
       redirect_to user
     else
       flash[:danger] = "Invalid email/password combination"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save :downcase_email
 
   validates :name, presence: true, length:{maximum: 50}
@@ -6,13 +8,32 @@ class User < ApplicationRecord
   validates :email, presence: true, length:{maximum:255},
     format:{with: VALID_EMAIL_REGEX},
     uniqueness:{case_sensitive: false}
-  validates :password, length:{minimum: 6}
+  validates :password, length:{minimum: 6}, allow_nil: true
 
   has_secure_password
+
 
   def User.digest string
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attributes remember_digest: nil
   end
 
   private
